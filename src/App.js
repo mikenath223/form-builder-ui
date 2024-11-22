003227520ef2681aad50f010c0612f2fb19cba00
Michgolden Ukeje <michgoldennathan17@gmail.com>
2024-11-22 09:42:27 +0100

    Handle text styling on home

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import FormBuilder from './components/FormBuilder';
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center py-8">
-      <h1 className="text-3xl font-bold text-blue-600 mb-8">Drag-and-Drop Form Builder</h1>
+      <h1 className="sm:text-3xl text-xl font-bold text-blue-600 mb-8 text-center">
+        Drag-and-Drop Form Builder
+      </h1>
       <FormBuilder />
     </div>
   );
